ebd9e2f07b1f7a5ca4d3c61aaab6585be664d9b2
Anjali Regis <anjaliregis@gmail.com>
2016-05-21 18:29:18 -0500

    The functions have been created to calculate/retrieve the inverse of a matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## We are creating a set of functions which calculates the inverse of a matrix and stores its value in the global environment
+## and retrives the inverse if the same matrix is entered to get the inverse.
+## makeCacheMatrix function will calculate and store the inverse of a matrix and then cacheSolve will retrieve it if it is stored already.
 
-## Write a short comment describing this function
+## makeCacheMatrix function will calculate and store the inverse of a matrix 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  CM<<-x       ## setting the CM= matrix A
+  Cacheinverse<<-solve(CM)  ## Caching the inverse of matrix A/CM
 }
 
 
-## Write a short comment describing this function
+## This function does the lexical scoping to see if the inverse of the matrix is already stored, if not then it calculates.
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x=matrix()) {
         ## Return a matrix that is the inverse of 'x'
+  secondmatrix<-x ##store the value of second matrix
+  Cacheinverse2<-NULL  ## setting the Cacheinverse2 = NULL
+  
+  
+  if ( (identical(CM,secondmatrix)) && (!is.null(Cacheinverse)) ) {  ##if there is previously cached inverse of identical matrix
+    
+    
+    message("getting cached data since the inverse of identical matrix is cached already") ## then retrieve those value
+    Cacheinverse
+  }
+  
+  else{                             ##Otherwise calculate fresh
+    Cacheinverse2 <-solve(secondmatrix)
+    message("Calculating the inverse of matrix since the supplied matrix is different or the value is not cached.")
+    Cacheinverse2
+  }
+  
 }
+
